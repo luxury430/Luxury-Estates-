882ceb5df21d92dbc90137a28e7418eced1ada83
luxury430 <cantwait037@gmail.com>
2026-03-30 07:55:48 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,57 @@
+// 🔥 Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBm_cuY0tQI60bAzwWZyvuUSaEX6sTgTs4",
+  authDomain: "neuroforge-815e5.firebaseapp.com",
+  projectId: "neuroforge-815e5",
+  storageBucket: "neuroforge-815e5.firebasestorage.app",
+  messagingSenderId: "101095135000",
+  appId: "1:101095135000:web:b5068cfb8262dd9f8c306d"
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// =======================
+// LOGIN
+// =======================
+const loginForm = document.getElementById("login");
+
+if (loginForm) {
+  loginForm.onsubmit = function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => alert("Login success 🔥"))
+      .catch(() => alert("Login failed ❌"));
+  };
+}
+
+// =======================
+// SIGNUP
+// =======================
+const signupForm = document.getElementById("signup");
+
+if (signupForm) {
+  signupForm.onsubmit = function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("signupEmail").value;
+    const password = document.getElementById("signupPassword").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => alert("Signup success 🚀"))
+      .catch(() => alert("Signup failed ❌"));
+  };
+}
